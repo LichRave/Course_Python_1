6c1ec5fc8e5c31ad243203955b4a3a6e38a466fa
Artem Monkiewicz <artem.monkiewicz@gmail.com>
2023-11-01 17:24:14 +0100

    os.system("git add .") os.system(f'git commit -m "{FirstCommit}"') os.system("git push")

---
+++ 01_intro.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  6 11:21:04 2023
+
+@author: Lich Rave
+"""
+# First comment
+
+#%%
+print (2 + 2)
+
+print(3 * 3)
+
+#%%
+print (3 + 2 * 2)
+print (4 - 2 * 2)
+
+#%%
+10 // 3 
+
+#%%
+2 ** 5
+
+# %%
+a = 10
+b = 20
+
+c = a * b
+
+# %%
+print(c)
+
+# %%
+print("love python")
+
+# %%
+import os
+os.getcwd()
+
+# %%
+print ("""
+Instrukcja uruchamiania pliku przyklad.py
+
+   --file [nazwa pliku]
+        zapisuje output do pliku
+        
+   --quiet
+        wycisza logi w konsoli
+Koniec.""")
+
+
+# %%
+text = "I love Python. "
+print (text)
+print (text * 3)
+print ('hau...' * 8)
+print('*' * 30)
+
+# %%
+'Python'
+'Py' 'thon'
+print ('Py''thon')
+
+# %%
+
+
+
---
+++ Zadania#1.py
@@ -0,0 +1,113 @@
+# 1
+print("Zadanie1")
+print("Разниц")
+print("Позов")
+
+# 2
+print("Zadanie2")
+my_text = "Hello, World!"
+print(my_text)
+
+# 3
+print("Zadanie3")
+my_text = "Hello, World!"
+print(type(my_text))
+print(type(-1))
+print(type(2.1))
+print(type(1))
+print(type(-1))
+
+# 4
+print("Zadanie4")
+glass_of_wather = 3
+print("I drank", glass_of_wather, "glasses of water today.")
+
+# 5
+print("Zadanie5")
+glass_of_wather = 3
+glass_of_wather = glass_of_wather + 1
+print("I drank", glass_of_wather, "glasses of water today.")
+
+# 6a-d
+print("Zadanie 6a CAŁKOWITA")
+x = 10
+print(type(x))
+
+print("Zadanie 6b ZMIENNOPRZECINKOWA")
+zmienna_2 = 3.14
+print(type(zmienna_2))
+
+print("Zadanie6c TEKST")
+zmienna_3 = "Tekst Test"
+print(type(zmienna_3))
+
+print("Zadanie6d Zmiana")
+zmienna_2 = 105
+print(type(zmienna_2))
+
+print("Zadanie6c LOGICZNE")
+x = True
+print(type(x))
+
+# 7
+print("Zadanie 7")
+my_grade = "10"
+answer_5 = float(my_grade)
+print(type(answer_5))
+
+# 8
+print("Zadanie 8")
+my_temp = 97.10
+answer_6 = int(my_temp)
+print(type(answer_6))
+
+# 9
+print("Zadanie 9")
+my_temp = 97.10678657
+answer_7 = round(my_temp, 2)
+print(answer_7)
+
+# 10
+print("Zadanie 10")
+lst = ["red", "green", "blue"]
+print(lst)
+
+# 11
+print("Zadanie 11")
+a = 10
+b = 3
+result = a / b
+print(result)
+
+# 12
+print("Zadanie 12")
+speed = 750
+speed += 100
+print(speed)
+
+# 13
+print("Zadanie 13")
+remainder = 1000 % 400
+print(remainder)
+
+# 14
+print("Zadanie 14")
+p_result = 11111**2
+print(p_result)
+
+
+# 15
+print("Zadanie 15")
+a = 450
+b = 500
+
+is_equal1 = a == b
+
+print(is_equal1)
+
+
+# 14
+print("Zadanie 14")
+p_result = 11111**2
+print(p_result)
+Time=3,16
