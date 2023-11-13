02245bef686d42fb021210f96b1bccf07ba1e24c
Artem Monkiewicz <artem.monkiewicz@gmail.com>
2023-11-13 23:45:19 +0100

    continuation of work, new elements

---
+++ dictionary.py
@@ -0,0 +1,42 @@
+# Słownik
+phonebook = {}
+
+# Dodać elementy
+phonebook["Jack"] = 1111111
+phonebook["Sarah"] = 222222
+
+print(phonebook)
+print(phonebook["Jack"])
+
+# Definicja gotowego słownika
+phonebook2 = {"John": 333333, "Dizee": 444444}
+print(phonebook2)
+
+# Przykład słownika
+human = {}
+
+human["name"] = "Maciej"
+human["height"] = 180
+human["weight"] = 90
+
+print(human)
+
+human_2 = {
+    "name": "Maciej",
+    "height": 180.5,
+    "weight": 90.5,
+    "Hobby": ["Vieo games", "programming", "mountain trips"],
+}
+print(human_2)
+
+print(human_2.get("hobby"))
+
+# usuwanie elementu ze slownika v1
+del human_2["Hobby"]
+print(human_2)
+# usuwanie elementu ze slownika v2
+x = human_2.pop("weight")
+print(human_2)
+
+
+# TIME------26 min
