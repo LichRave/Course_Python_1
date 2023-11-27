af3ef923d09fd9cc39e16206d009f360c8b79c29
Artem Monkiewicz <artem.monkiewicz@gmail.com>
2023-11-27 08:02:59 +0100

    Mutable_Immutable

---
+++ Mutable_Immutable.py
@@ -0,0 +1,20 @@
+# Modyfikowalne (mutable) i niemodyfikowalne (immutable) typy
+# Oto krókie podsumowanie dzielące zmienne na modyfikowalne i niemodyfikowalne:
+
+# Mutable
+# Zawartość raz utworzonych zmiennych takiego typu, można modyfikować bez konieczności tworzenia nowej zmiennej - oszczędność pamięci, kolekcje te mogą być naprawdę duże, zwracanie za każdym razem nowej sekwencji mogłoby doprowadzić do problemów z pamięcią.
+
+# ------- lista,
+# ------- zbiór,
+# ------- słownik.
+
+
+# Immutable
+# Zmiennych tego typu nie można modyfikować po utworzeniu, za każdym razem zostanie utworzony nowy byt.
+
+# ------- int,
+# ------- float,
+# ------- bool,
+# ------- str,
+# ------- tuple,
+# ------- frozenset.
